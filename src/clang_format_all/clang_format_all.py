dcf176bc792652184ecad1b98828c28fe78612ae
thebigg <yunior.eury@gmail.com>
2022-07-11 22:56:12 -0500

    -Add exclude_dirs

--- src/clang_format_all/clang_format_all.py
+++ src/clang_format_all/clang_format_all.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import argparse
 import logging
 
+import yaml
+
 logging.basicConfig()
 logger = logging.getLogger('check-all')
 logger.setLevel(logging.INFO)
@@ -15,7 +17,10 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='clang_format_all starts at this directory and drills down recursively')
     parser.add_argument('--root_dir', type=str, required=True,
-                        help='Directory to start running clang-format at.')
+                        help='Directory to start running clang-format at.', default=[])
+
+    parser.add_argument('--exclude_dirs', type=str, required=False,
+                        help='Excludes yaml with directories to ignore.')
 
     parser.add_argument('--check_all',
                         help='Only check files without modifying them. Returns non-zero value on failure;'
@@ -55,19 +60,30 @@ def format_all_walk_recursive(root_dir: str):
             path = Path(os.path.join(root, file))
             if is_cpp_or_c_file(path):
                 if subprocess.run(["clang-format", "--Werror", "-style=file",
-                                   path],capture_output=True).returncode != 0:
+                                   path], capture_output=True).returncode != 0:
                     logger.info("\"%s\": An error occurred while parsing this file.", path)
                     exit(1)
                 else:
                     logger.info("\"%s\": parsed successfully.", path)
 
 
+def get_resolved_paths(unresolved_paths: [str]) -> [str]:
+    resolved_paths = []
+    for p in unresolved_paths:
+        resolved_paths.append(Path(p).resolve())
+    return resolved_paths
+
+
 def main():
     args = parse_args()
+    if args.exclude_dirs:
+        with open(args.exclude_dirs) as yaml_stream:
+            excludes = yaml.load(yaml_stream, Loader=yaml.CSafeLoader)
+            if 'exclude_dirs' not in excludes:
+                logger.error(f"No key 'exclude_dirs' found in {args.exclude_dirs}")
+                return -1
+
     if args.check_all:
         check_all_walk_recursive(args.root_dir)
     else:
         format_all_walk_recursive(args.root_dir)
-
-
-main()
